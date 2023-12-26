9d04456fd6c6a007b3df289fdf7211406daa0a22
Kijanaa <irene.yanapouth@gmail.com>
2023-12-26 22:40:29 +0100

    first commit

--- App.js
+++ App.js
@@ -4,13 +4,43 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ma cooo c'est pas grave, Mais ne baisse pas les bras !
+        Hello comment tu vas ?
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+
+
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ma cooo c'est pas grave, Mais ne baisse pas les bras !
+        Hello comment tu vas ?
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styless = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
