54065afbde55a3fa3c5324419b5958067898c475
Kijanaa <irene.yanapouth@gmail.com>
2024-01-01 23:15:29 +0100

    quelques changement mineures

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text , StyleSheet} from "react-native";
 
 function App () {
   return (
-    <View style = {{ marginTop: 50, flexDirection: "row", backgroundColor: 'lightgreen'}}>
-      <Text> Text One</Text>
-      <Text> Gars, affiche toi non ? massa !!</Text>
+    <View style = {{ marginTop: 50, flexDirection: "column"}}>
+      <View style={{ backgroundColor: 'green'}}>
+        <Text style={{ fontFamily: 'cochin'}}> Text One </Text>
+        <Text style={{ fontFamily: 'cochin'}}> Text Two </Text>
+      </View>
+
+      <View style={{ backgroundColor: 'red'}}>
+        <Text style={{ fontFamily: 'cochin', fontSize: '20', fontWeight:'bold'}}> Text Three </Text>
+      </View>
+      
     </View>
   );
 }
+// const styles = StyleSheet
+
+
+
 export default App;
 
 
