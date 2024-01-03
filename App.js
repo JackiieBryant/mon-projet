ca68cb9a48975e95099ef8ad433becb2c3ad9e80
Kijanaa <irene.yanapouth@gmail.com>
2024-01-03 21:56:04 +0100

    add git

--- App.js
+++ App.js
@@ -4,19 +4,18 @@ import { View, Text , StyleSheet} from "react-native";
 function App () {
   return (
     <View style = {{ marginTop: 50, flexDirection: "column"}}>
-      <View style={{ backgroundColor: 'green'}}>
-        <Text style={{ fontFamily: 'cochin'}}> Text One </Text>
-        <Text style={{ fontFamily: 'cochin'}}> Text Two </Text>
-      </View>
+        {/* <View style={{ backgroundColor: 'green'}}> */}
+          <Text style={{ fontFamily: 'cochin'}}> Text One </Text>
+          <Text style={{ fontFamily: 'cochin'}}> Text Two </Text>
+        </View>
 
       <View style={{ backgroundColor: 'red'}}>
         <Text style={{ fontFamily: 'cochin', fontSize: '20', fontWeight:'bold'}}> Text Three </Text>
       </View>
-      
     </View>
   );
 }
-// const styles = StyleSheet
+const styles = StyleSheet
 
 
 
