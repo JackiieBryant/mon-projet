2efdccea3dc0ff52fbd7bda32bffe39539a9e74e
Kijanaa <irene.yanapouth@gmail.com>
2024-01-06 23:20:31 +0100

    hey

--- App.js
+++ App.js
@@ -2,17 +2,7 @@ import React from "react";
 import { View, Text , StyleSheet} from "react-native";
 
 function App () {
-  return (
-    <View style = {{ marginTop: 50, flexDirection: "column"}}>
-        {/* <View style={{ backgroundColor: 'green'}}> */}
-          {/* <Text style={{ fontFamily: 'cochin'}}> Text One </Text> */}
-          <Text style={{ fontFamily: 'cochin'}}> Text Two </Text>
-        </View>
-
-      // <View style={{ backgroundColor: 'red'}}>
-        <Text style={{ fontFamily: 'cochin', fontSize: '20', fontWeight:'bold'}}> Text Three </Text>
-      </View>
-    </View>
+  return (      
   );
 }
 const styles = StyleSheet
